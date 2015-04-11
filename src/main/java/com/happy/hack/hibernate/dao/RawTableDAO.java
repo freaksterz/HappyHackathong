9151df5b007d9f2171326ef4225bff3738065e19
freaksterz <vickyknowsu@gmail.com>
2015-04-11 20:19:37 +0530

    commit List data

--- src/main/java/com/happy/hack/hibernate/dao/RawTableDAO.java
+++ src/main/java/com/happy/hack/hibernate/dao/RawTableDAO.java
@@ -2,13 +2,15 @@ package com.happy.hack.hibernate.dao;
 
 import com.happy.hack.hibernate.entity.RawTable;
 
+import java.util.List;
+
 /**
  * Created by freakster on 11/4/15.
  */
 public interface RawTableDAO {
 
     void addRow(RawTable rw);
-    String getColumn(String columnname);
+    List<RawTable> getAllData();
 
 
 
--- src/main/java/com/happy/hack/hibernate/dao/RawTableDAOImpl.java
+++ src/main/java/com/happy/hack/hibernate/dao/RawTableDAOImpl.java
@@ -4,8 +4,12 @@ package com.happy.hack.hibernate.dao;
 import com.happy.hack.hibernate.entity.RawTable;
 import com.salesorderapp.common.util.HibernateUtil;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+
+import java.util.List;
 
 /**
  * Created by freakster on 11/4/15.
@@ -22,11 +26,14 @@ public class RawTableDAOImpl implements RawTableDAO {
     }
 
     @Override
-    public String getColumn(String columnname) {
+    public List<RawTable> getAllData() {
 
-
-        return null;
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from RawTable");
+        List<RawTable> rowList = query.list();
+        return rowList;
     }
 
-	
+
 }
